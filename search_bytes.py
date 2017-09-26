42cacc422616fca507be27c994289007efb08c8d
Antttooon <anttoon@bk.ru>
2017-09-26 12:33:25 +0300

    MultiSearch
    Расширение функции - поиск всех элементов

--- search_bytes.py
+++ search_bytes.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 def search(file_name, string):
     """
-    функция ищет байтовую подстроку в файле
+    функция ищет все байтовые подстроки в файле
     :param file_name: имя файла - str
     :param string: искомая строка - bytes
     :return: При наличии подстроки в файле  печатает "Yes" и точку первого вхождения
@@ -11,12 +11,23 @@ def search(file_name, string):
 
     start = datetime.now()
     with open(file_name, 'rb') as f:
-        data = f.read()
         # print('\n',data[1000:1010])
-        if string in data:
-            print('Yes')
-            print(data.find(string))
+        lenght = 0
+        for i in f.readlines():
+            if string in i:
+                pos = lenght+i.find(string)
+                print('Yes')
+                print('Позиция от начала файла', pos)
+            lenght += len(i)
     end = datetime.now()
     print(end - start,'\n')
 
-search('file_image.jpg',b'\x08\x08\x08\t\x08\x0c\t\t\x0c\x11') 
+search('file_image.jpg',b'\x08\x08\x08\t\x08\x0c\t\t\x0c\x11')
+
+# ________________________________test____________________________________
+
+# with open('file_image.jpg', 'rb') as f:
+#     print(f.read()[1000:1010])
+
+# with open('file_image.jpg', 'rb') as f:
+#     print(f.read()[12385:12395])
